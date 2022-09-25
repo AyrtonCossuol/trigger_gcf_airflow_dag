0e25bcece3fa0b34f1d3f6e6f2558f46cea2a4aa
AyrtonCossuol <ayrton.cossuol@live.com>
2022-09-25 17:27:20 -0300

    :construction: final version

--- dags/move_file_buckets.py
+++ dags/move_file_buckets.py
@@ -2,7 +2,7 @@ from airflow import DAG
 from airflow.providers.google.cloud.operators.gcs import GCSListObjectsOperator
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.providers.google.cloud.transfers.gcs_to_gcs import GCSToGCSOperator
-
+from datetime import datetime
 from os import getenv
 
 BUCKET_NAME_RECEPTION   = getenv('BUCKET_NAME_RECEPTION', 'reception-bucket')
@@ -21,6 +21,7 @@ with DAG(
     dag_id = 'move_file_buckets',
     tags = ['development', 'cloud storage', 'movimentation'],
     default_args = default_args,
+    start_date = datetime(year=2022, month=9, day=24),
     schedule_interval = None,
     catchup = False
 ) as dag:
@@ -40,6 +41,6 @@ with DAG(
     )
 
     initial_task = DummyOperator(task_id = 'initial_task')
-    finish_task = DummyOperator(task_id = 'finish_task')
+    finish_task  = DummyOperator(task_id = 'finish_task')
 
     initial_task >> move_files >> list_files_move >> finish_task
